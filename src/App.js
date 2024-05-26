1f0faa246a6efa973a4dcac980fc2105d2d27bae
duttmistry <dutt.cybercom@gmail.com>
2024-05-26 13:45:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [userClient, setUserClient] = useState("");
+  useEffect(() => {
+    setUserClient(detectOS());
+  }, []);
+
+  function detectOS() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Windows Phone detection
+    if (/windows phone/i.test(userAgent)) {
+      return "Windows Phone";
+    }
+
+    // Windows detection
+    if (/windows/i.test(userAgent) && !/windows phone/i.test(userAgent)) {
+      return "Windows";
+    }
+
+    // iOS detection
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return "iOS";
+    }
+
+    // Android detection
+    if (/android/i.test(userAgent)) {
+      return "Android";
+    }
+
+    return "unknown";
+  }
+
+  console.log(detectOS());
+
+  // console.log(detectOS());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {userClient !== "unknown" ? (
+        <div>You Are Using - {userClient}</div>
+      ) : (
+        <div>Your Device is Not Detecting ({userClient})</div>
+      )}
     </div>
   );
 }
